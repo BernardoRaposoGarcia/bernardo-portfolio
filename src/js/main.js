f703251a6223baf5940c477dc5ac50f8277d4045
BernardoRaposoGarcia <bernardoraposomonteiro@gmail.com>
2024-07-03 16:34:52 -0300

    Config vite, bootstrap and sass

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// Import our custom CSS
+import '../css/styles.min.css'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// Import Individually plugins
+// import Alert from 'bootstrap/js/dist/alert';
+
+// or, specify which plugins you need:
+// import { Tooltip, Toast, Popover } from 'bootstrap';
